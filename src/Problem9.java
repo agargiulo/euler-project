99d0b9da77c877e0c129ad5484a9e410ba23a4ad
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-05-12 17:01:35 -0400

    Starting #9

---
+++ src/Problem9.java
@@ -0,0 +1,28 @@
+/**
+ * @author Anthony Gargiulo
+ * @version 0.1
+ *          Euler Project
+ *          Problem 9
+ *          ---------
+ *          A Pythagorean triplet is a set of three natural numbers, a < b < c,
+ *          for which,
+ *          a^2 + b^2 = c^2
+ *          For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *          There exists exactly one Pythagorean triplet for which a + b + c =
+ *          1000.
+ *          Find the product abc.
+ */
+
+public class Problem9
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
