52a8365b97f25fa105d86ee0e6e6265c4d7d7c93
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-05-13 05:04:03 -0400

    Commented the PrimeGen so it is easier to understand

--- src/PrimeGen.java
+++ src/PrimeGen.java
@@ -32,22 +32,31 @@ public class PrimeGen
     public static boolean isPrime(Long num)
     {
         if (num <= 1)
-        {
+        {// 1 is never prime
             return false;
         } else if (num < 4)
-        {
+        {// 2 and 3 are prime...
             return true;
         } else if (num % 2 == 0)
-        {
+        {// any even number is not prime
             return false;
         } else if (num < 9)
-        {
+        {// the previous tests have already dealt with 4, 6, 8
             return true;
         } else if (num % 3 == 0)
-        {
+        {// any number divisible by three is clearly not prime
             return false;
         } else
         {
+            /*
+             * basically, this part deals with the fact that all primes over 3
+             * satisfy the equation:
+             * prime = 6k +/- 1
+             * Also the fact that a number will not have any prime factors over
+             * the square root of that number, except for itself if it is prime
+             */
+            // Math.sqrt(num) rounded to the greatest int, r, such that r * r <=
+            // num
             int r = (int) Math.floor(Math.sqrt(num));
             int f = 5;
             while (f <= r)
