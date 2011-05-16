513ef527ed883fb0fd2edeb19d9dd6744f8257fe
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-05-15 22:58:56 -0400

    starting problem #48. And i think eclipse changed all the files to have the *nix EOL instead of the windows one

---
+++ src/Problem48.java
@@ -0,0 +1,23 @@
+/**
+ * @author Anthony Gargiulo
+ * @version 0.1
+ *          Euler Project
+ *          Problem 48
+ *          ---------
+ *          The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ *          Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... +
+ *          1000^1000.
+ */
+
+public class Problem48
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+}
