082cbf85b3fadfd14f4c6e453078e285ba31aafd
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-05-18 18:27:33 -0400

    Started to work on #15, and did some more work on #9. Can't get too far without Internet access. (also, moving between Windows and Fedora causes Eclipse to change line endings. Thus updating EVERYTHING)

---
+++ src/Problem15.java
@@ -0,0 +1,61 @@
+/**
+ * @author Anthony Gargiulo
+ * @version 0.1
+ *          Euler Project
+ *          Problem s15
+ *          ---------
+ *          Starting in the top left corner of a 2×2 grid, there are 6 routes
+ *          (without backtracking) to the bottom right corner.
+ *          How many routes are there through a 20×20 grid?
+ */
+
+public class Problem15
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+}
+
+/**
+ * @author Anthony Gargiulo
+ */
+class Node
+{
+    /**
+     * 
+     */
+    private final Node[] neighbors;
+
+    /**
+     * @param south
+     * @param east
+     */
+    public Node(Node south, Node east)
+    {
+        neighbors = new Node[2];
+        neighbors[0] = south;
+        neighbors[1] = east;
+    }
+
+    /**
+     * @return the eastern neighbor
+     */
+    public Node getEast()
+    {
+        return neighbors[1];
+    }
+
+    /**
+     * @return the southern neighbor
+     */
+    public Node getSouth()
+    {
+        return neighbors[0];
+    }
+}
--- src/Problem9.java
+++ src/Problem9.java
@@ -21,32 +21,12 @@ public class Problem9
      */
     public static void main(String[] args)
     {
-        int upperBound = 100;
-        int a = 2;
-        int b = 1;
-        while (true)
+        final int SUM = 1000;
+        int a = 1;
+        int b = 2;
+        int c = (int) Math.sqrt(a * a + b * b);
+        while (a + b + c != SUM)
         {
-            double c = Math.sqrt(a * a + b * b);
-            if (c != (int) c)
-            {
-                b++;
-                if (b + 1 == a)
-                {
-                    continue;
-                } else
-                {
-                    b = 1;
-                    a++;
-                    if (a == upperBound)
-                    {
-                        break;
-                    }
-                }
-            } else
-            {
-                System.out.println(String.format("a: %s | b: %s | c: %s", a, b,
-                        Math.sqrt(a * a + b * b)));
-            }
         }
     }
 }
