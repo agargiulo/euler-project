4a610df3b2de9c6c5d2571922ec9600b2c479e89
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2011-06-18 00:56:41 -0400

    I really hate windows line endings...

--- src/Problem015.java
+++ src/Problem015.java
@@ -4,9 +4,9 @@
  *          Euler Project
  *          Problem s15
  *          ---------
- *          Starting in the top left corner of a 2×2 grid, there are 6 routes
+ *          Starting in the top left corner of a 2x2 grid, there are 6 routes
  *          (without backtracking) to the bottom right corner.
- *          How many routes are there through a 20×20 grid?
+ *          How many routes are there through a 20x20 grid?
  */
 
 public class Problem015
