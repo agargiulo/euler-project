3a44984ecdf7cd24b94535f634e52d33258fc0c8
Anthony Gargiulo <agargiulo@anthonygargiulo.info>
2012-03-26 14:22:29 -0400

    fixed formatting of a comment, minor edit

--- src/Problem002.java
+++ src/Problem002.java
@@ -12,8 +12,7 @@ import java.util.ArrayList;
  *		  two terms. By starting with 1 and 2, the first 10 terms will be:
  *		  1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
  *		  By considering the terms in the Fibonacci sequence whose values do
- *		  not
- *		  exceed four million, find the sum of the even-valued terms.
+ *		  not exceed four million, find the sum of the even-valued terms.
  */
 
 public class Problem002
