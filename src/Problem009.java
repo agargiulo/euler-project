6d84b069a1307a047b314379e489e93a4afdbf85
Anthony Gargiulo <anthony@agargiulo.com>
2012-03-29 14:38:53 -0400

    FINALLY figured out a java solution. I was overthinking it, this way is really nice and simple.

--- src/Problem009.java
+++ src/Problem009.java
@@ -1,6 +1,8 @@
 /**
+ * **SOLVED**
+ * 
  * @author Anthony Gargiulo
- * @version 0.1
+ * @version 1.0
  *          Euler Project
  *          Problem 9
  *          ---------
@@ -16,19 +18,26 @@
 public class Problem009
 {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        final long SUM = 1000;
-        long a, b, c;
-        for (long n = 3; n < 35; n += 2)
-        {
-            a = n;
-            c = (a * a + 1)/2;
-            b = c - 1;
-            System.out.println("a: " + a + "\t|\tb: " + b + "\t|\tc: " + c + "\t|\tSum: " + (a+b+c));
-        }
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		final int SUM = 1000;
+		int a, b, c;
+		for (a = 1; a < SUM / 2; a++)
+		{
+			for (b = a + 1; b < SUM; b++)
+			{
+				c = SUM - a - b;
+				if (a * a + b * b == c * c)
+				{
+					System.out.println("a: " + a + " b: " + b + " c: " + c);
+					System.out.println("a*b*c: " + a * b * c);
+
+				}
+			}
+		}
+
+	}
 }
