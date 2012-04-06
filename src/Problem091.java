be957a30dc8b760b43cf8e8a201c9b55870010b0
Anthony Gargiulo <anthony@agargiulo.com>
2012-04-06 14:53:54 -0400

    Fixed an issue with the java compiler. Windows was okay with the fancy x, but linux was not =[

--- src/Problem091.java
+++ src/Problem091.java
@@ -18,7 +18,7 @@ import java.math.BigInteger;
  *          contain more digits.
  * 
  *          However, in 2004 there was found a massive non-Mersenne prime which
- *          contains 2,357,207 digits: 28433×(2^7830457)+1.
+ *          contains 2,357,207 digits: 28433*(2^7830457)+1.
  * 
  *          Find the last ten digits of this prime number.
  * 
