f44970cb24355c652f53feb34ec1aa6de2ac29e5
Anthony Gargiulo <anthony@agargiulo.com>
2013-07-27 19:11:00 -0400

    actually solved #24 now, this is much much much faster

--- src/com/agargiulo/code/projecteuler/Problem024.java
+++ src/com/agargiulo/code/projecteuler/Problem024.java
@@ -1,66 +1,51 @@
+
 package com.agargiulo.code.projecteuler;
-import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * File: Problem024.java
  * Date created: Jul 26, 2013
  * 
+ * **SOLVED**
+ * 
  * @author agargiulo
+ * @version 1.0
+ *          A permutation is an ordered arrangement of objects. For example,
+ *          3124
+ *          is one possible permutation of the digits 1, 2, 3 and 4. If all of
+ *          the permutations are listed numerically or alphabetically, we call
+ *          it
+ *          lexicographic order. The lexicographic permutations of 0, 1 and 2
+ *          are:
+ * 
+ *          012 021 102 120 201 210
+ * 
+ *          What is the millionth lexicographic permutation of the digits 0, 1,
+ *          2, 3, 4, 5, 6, 7, 8 and 9?
  */
 
-/**
- * @version 0.5
- * @author agargiulo
- *         A permutation is an ordered arrangement of objects. For example, 3124
- *         is one possible permutation of the digits 1, 2, 3 and 4. If all of
- *         the permutations are listed numerically or alphabetically, we call it
- *         lexicographic order. The lexicographic permutations of 0, 1 and 2
- *         are:
- * 
- *         012 021 102 120 201 210
- * 
- *         What is the millionth lexicographic permutation of the digits 0, 1,
- *         2, 3, 4, 5, 6, 7, 8 and 9?
- * 
- */
 public class Problem024
 {
 
-	private static ArrayList<Integer>	permutations;
+	static long	numOfPermutations;
 
-	private static int arrToInt(int[] digits)
+	private static void getPermutations(String begDigits, String endDigits)
 	{
-		/*
-		 * [1,2,3,4,5] ==> 12345
-		 * 1 * 10^(arr.len -1)
-		 */
-		int powOfTen, ret = 0;
-		for (int i = 0; i < digits.length; i++)
+		if (endDigits.length() <= 1)
 		{
-			powOfTen = digits.length - (i + 1);
-			ret += digits[i] * (int) Math.pow(10, powOfTen);
-		}
-		return ret;
-
-	}
-
-	private static void getPermutations(int[] digits, int index)
-	{
-		if (index == digits.length)
-		{
-			if (!permutations.contains(arrToInt(digits)))
+			numOfPermutations++;
+			if (numOfPermutations >= 1000000)
 			{
-				permutations.add(arrToInt(digits));
+				System.out.println(begDigits + endDigits);
+				System.exit(0);
 			}
-			return;
-		}
-		for (int i = index; i < digits.length; i++)
+		} else
 		{
-			int[] permutation = digits.clone();
-			permutation[index] = digits[i];
-			permutation[i] = digits[index];
-			getPermutations(permutation, index + 1);
+			for (int i = 0; i < endDigits.length(); i++)
+			{
+				String newDigits = endDigits.substring(0, i)
+						+ endDigits.substring(i + 1);
+				getPermutations(begDigits + endDigits.charAt(i), newDigits);
+			}
 		}
 	}
 
@@ -69,14 +54,8 @@ public class Problem024
 	 */
 	public static void main(String[] args)
 	{
-		permutations = new ArrayList<Integer>(3628800);
-		int[] digits =
-		{0,1,2,3,4,5,6,7,8,9};
-		getPermutations(digits, 0);
-		Collections.sort(permutations);
-		System.out
-				.print("The millionth lexicographic permutation of 0123456789 is ");
-		System.out.println(permutations.get(999999));
+		numOfPermutations = 0;
+		String digits = "0123456789";
+		getPermutations("", digits);
 	}
-
 }
